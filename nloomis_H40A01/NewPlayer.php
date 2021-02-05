484f62ce690df0a2f818416176c6376f23417e86
Noah Loomis <56049624+NoahMLoomis@users.noreply.github.com>
2021-02-05 14:56:37 -0500

    Added todo and folder strucures

--- nloomis_H40A01/NewPlayer.php
+++ nloomis_H40A01/NewPlayer.php
@@ -13,49 +13,52 @@
 
 </head>
 <body>
+
+    
     <form action="" method="get" name="myForm">
     <h1>Add a new player</h1>
-    <div>
         <p id="errorFirstName" class="error"></p>
-    </div>
-    <div>
-        <br/>
         <label for="firstName" id="fName" class="label">First Name: </label>
-        <input type="text" id="firstName" name="firstName"/><br/><br/>
-    </div>
-    <div>
+        <input type="text" id="firstName" name="firstName"/><br/>
+
         <p id="errorLastName" class="error"></p>
-    </div>
-    <div>
         <label for="lastName" id="lName" class="label">Last Name: </label>
-        <input type="text" id="lastName" name="lastName"/><br/><br/>
-    </div>
-    <div>
-        <p id="errorPhone" class="error"></p>
-    </div>
-    <div>
-        <label for="phone" id="phoneNum" class="label">Phone Number: </label>
-        <input type="text" id="phone" name="phone"/><br/><br/>
-    </div>
+        <input type="text" id="lastName" name="lastName"/><br/>
 
-    <div>
-        <p id="errorCity" class="error"></p>
-    </div>
-    <div>
-        <label for="city" id="cit" class="label">City: </label>
-        <input type="text" id="city" name="city"/><br/><br/>
-    </div>
-
-    <div>
         <p id="errorEmail" class="error"></p>
-    </div>
-    <div>
         <label for="email" id="mail" class="label">Email: </label>
         <input type="text" id="email" size="20" name="email"/>
-    </div>
-    <div>
+
+        <p id="errorCity" class="error"></p>
+        <label for="city" id="cit" class="label">City: </label>
+        <select id="city" name="city">
+            <option value="Canada">Canada</option>
+            <option value="USA">United States</option>
+            <option value="Argentina">Argentina</option>
+            <option value="Brazil">Brazil</option>
+            <option value="Ecuador">Ecuador</option>
+            <option value="Peru">Peru</option>
+            <option value="Poland">Poland</option>
+        </select>
+        <br/>
+
+        <label for="professional" id="professionallbl" class="label">Professional: </label>
+        <input type="checkbox" id="professional" name="professional"/>
+        
+        <p id="errorPhone" class="error"></p>
+        <label for="phone" id="phoneNum" class="label">Phone Number: </label>
+        <input type="text" id="phone" name="phone"/><br/>
+
+        <p id="errorPassword" class="error"></p>
+        <label for="password" id="passwordlbl" class="label">Password: </label>
+        <input type="text" id="password" name="professional"/>
+
+        <p id="errorConfirmPassword" class="error"></p>
+        <label for="confirmPassword" id="confirmPasswordlbl" class="label">Confirm Password: </label>
+        <input type="text" id="confirmPassword" name="confirmPassword"/>
+        
+        <br/>
         <input type="submit" class="btn btn-success" value="Submit" id="sub"/>
-    </div>
     </form>
 
 
---
+++ nloomis_H40A01/functions/myFunctions.php
@@ -0,0 +1,15 @@
+<?php
+
+   //TODO
+    // - Check all Reg
+    // - Give specific err messages
+    // - Test passwords match
+
+    // Probably a way to test this without regex
+    $emptyReg= "/^\s*$/"; 
+    $nameReg = "/[A-Za-z][-'\s]/";
+    $emailReg = "/^\w+[\w-.]*@\w+(-\w+)|(\w*)\.(com|org|ca)$/";
+
+    
+
+?>
